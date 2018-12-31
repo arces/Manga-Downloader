48f793f29c8f67e841ae4280cdbcb96aff7afec3
Dan <thatcoolemailat@yahoo.com>
2018-12-30 21:39:10 -0800

    New GUI Options
    Added a GUI with 2 options for PDF.py
    Fixed some test print outs and cleaned things up a little

--- README.md
+++ README.md
@@ -7,12 +7,12 @@ The PDF script will add the completed files into a pdf
 ## You will need the following packages to get it working:
 
 ```
-Python 3.x
-pillow (Python package)
-requests (Python package)
-requests-html (Python package)
-beautifulsoup4 (Python package)
-FPDF (Python package)
+Python 3.x (Tested with 3.7-32)
+pillow (Tested with  5.3.0)
+requests (Tested with 2.21.0)
+requests-html (Tested with 0.9.0)
+beautifulsoup4 (Tested with 4.6.3)
+FPDF (Tested with 1.7.2)
 ```
 
 
@@ -21,7 +21,37 @@ If you don't know how to install a python package then just type in
 pip install *Python_Package_name_here*
 ```
 
+
+
 ## Usage:
-Just run both of them from the command line. 
-Photodownloader.py will make folders and dl to those folders were it was called from
-pdf.py will look for those folders and take the images from them and make a pdf for each chapter.
+Just run both of them from the command line.
+ 
+<b>photodownloader.py</b> will make folders and download to those folders were it was called from.
+
+1.Enter in a URL that is similar in format to the default one.
+
+
+<b>pdf.py</b> will look for those folders and take the images from them and make a pdf based off of the options selected.
+
+1. Check "First Page Blank?" if you would like the first page of each pdf to be a blank page (Useful to know when a new chapter might start)
+2. Check "Combo" if you would like to add all the chapters into one large PDF called "combo.pdf" (<b>See known bugs below and FAQ</b>)
+
+##Known Bugs:
+1. Due to how the algorithm in the PDF.py file is, it will not recognize multiples of 10.
+<b>Ex</b>: Out of the this list of chapters [1,2,3,4,5,6,7,8,9,10,11,12,13] it will put it in the following order 
+ [1,10,11,12,13,2,3,4,5,6,7,8,9] if combo checkbox is selected.
+ 
+
+## FAQ:
+<b>The program is frozen, I can't click anything!</b> 
+>Don't worry this is normal. Due to how the code is written, it will be frozen until it is finished.
+
+<b>The images are not downloading! Why?</b>
+>There might be a couple reasons. If the program is finished running and it missed images. This means that the list of domains for the image server is out of date. You will need to look at the HTML source code and see what the domain where the images are hosted. You can add that into the program (Look for all the if statements). <b>PLEASE LET ME KNOW IF YOU FIND DOMAINS THAT ARE NOT IN THE LIST</b>
+
+<b>How can I help improve the program?</b>
+>Report any bugs you find! You can also suggest features or even code them yourself and do a pull request!
+
+<b>Ok... If combo is broken, how do I make them into one pdf?</b>
+>Use a program like Adobe Acrobat or SodaPDF to merge/combine pdfs together. I do that and it works perfect for me.
+
--- pdf.py
+++ pdf.py
@@ -1,13 +1,19 @@
 from fpdf import FPDF
 import os
+from tkinter import *
 import sys
 
-folders = []
-chapters = []
-images = []
-errors = []
-num_errors = 0
-folder_name = ""
+
+
+root = Tk()
+tf = Frame(root)
+bf = Frame(root)
+root.title("PDF Maker")
+
+checkBlank = IntVar()
+checkCombo = IntVar()
+checkBlank.set(0)
+checkCombo.set(0)
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
@@ -16,53 +22,133 @@ def get_immediate_subdirectories(a_dir):
     return [name for name in os.listdir(a_dir)
             if os.path.isdir(os.path.join(a_dir, name))]
 
+def main(blank, combo):
+    folders = []
+    chapters = []
+    images = []
+    errors = []
+    num_errors = 0
+    folder_name = ""
+    folders = (get_immediate_subdirectories(os.getcwd()))
+    #Debug print
+    #print(folders)
+    #print("these are the folders ^")
+    if combo==0:
+        for f in folders:
+            if (f[:2] == "Ch" or f[:3] =='vol' or f[:2] == "ch"):
+                images = []
+                image_names = []
+                images = os.listdir(os.path.join(os.getcwd() + "\\" + f))
+                #Debug prints
+                #print(images)
+                #print("These are the images")
+                #print(f)
+                for img in images:
+                    image_names.append(img[:-4])
+                i = 1
 
-folders = (get_immediate_subdirectories(os.getcwd()))
-#Debug print
-#print(folders)
-#print("these are the folders ^")
-for f in folders:
-    if (f[:2] == "Ch" or f[:3] =='vol' or f[:2] == "ch"):
-        images = []
-        image_names = []
-        images = os.listdir(os.path.join(os.getcwd() + "\\" + f))
-        #Debug prints
-        #print(images)
-        #print("These are the images")
-        #print(f)
-        for img in images:
-            image_names.append(img[:-4])
-        i = 1
-        #print(image_names)
+                #print(image_names)
+                pdf = FPDF()
+
+                #If the user checks the blank page box then there will be a blank page
+                if blank==1:
+                    pdf.add_page()
+
+                #print(images.__len__())
+                while (i < images.__len__()):
+                    try:
+                        pdf.set_font('Arial', 'B', 16)
+                        #print(i)
+                        full_image_name = str(images[image_names.index(str(i))])
+                        image_path = os.path.join(os.getcwd() + "\\" + f + "\\" + full_image_name)
+                        pdf.add_page()
+                        #Debug print
+                        #print("adding image "+full_image_name)
+                        pdf.image(image_path, x=0, y=0, w=190, )
+
+                    except:
+                        #print("Unexpected error:", sys.exc_info()[0])
+                        num_errors=num_errors+1
+                        full_image_name = str(i)
+                        #errors.append(full_image_name + " from "+f)
+                        #pdf.cell(40, 10, "Error adding image " + full_image_name + " from " + f)
+                        #print("Error adding image " + full_image_name + " from " + f)
+                    i = i + 1
+                try:
+                    pdf.close()
+                    pdf.output(f + ".pdf", "F")
+                except:
+                    print("Error closing/Saving PDF :'(")
+
+        if num_errors>0:
+            print("Finished with "+str(num_errors))
+        else:
+            print("Finished with no errors :D")
+    else:
         pdf = FPDF()
-        pdf.add_page()
-        #print(images.__len__())
-        while (i < images.__len__()):
-            try:
-                pdf.set_font('Arial', 'B', 16)
-                #print(i)
-                full_image_name = str(images[image_names.index(str(i))])
-                image_path = os.path.join(os.getcwd() + "\\" + f + "\\" + full_image_name)
-                pdf.add_page()
-                #Debug print
-                #print("adding image "+full_image_name)
-                pdf.image(image_path, x=0, y=0, w=190, )
+        pdf.set_font('Arial', 'B', 16)
+        for f in folders:
+            if (f[:2] == "Ch" or f[:3] == 'vol' or f[:2] == "ch"):
+                images = []
+                image_names = []
+                images = os.listdir(os.path.join(os.getcwd() + "\\" + f))
+                # Debug prints
+                # print(images)
+                # print("These are the images")
+                # print(f)
 
-            except:
-                #print("Unexpected error:", sys.exc_info()[0])
-                num_errors=num_errors+1
-                full_image_name = str(i);
-                #errors.append(full_image_name + " from "+f)
-                #pdf.cell(40, 10, "Error adding image " + full_image_name + " from " + f)
-                #print("Error adding image " + full_image_name + " from " + f)
-            i = i + 1
+                for img in images:
+                    image_names.append(img[:-4])
+                i = 1
+                # print(image_names)
+
+                # If the user checks the blank page box then there will be a blank page
+                if blank == 1:
+                    pdf.add_page()
+
+                print(images.__len__())
+                while (i < images.__len__()):
+                    try:
+
+                        # print(i)
+                        full_image_name = str(images[image_names.index(str(i))])
+                        image_path = os.path.join(os.getcwd() + "\\" + f + "\\" + full_image_name)
+                        pdf.add_page()
+                        # Debug print
+                        # print("adding image "+full_image_name)
+                        pdf.image(image_path, x=0, y=0, w=190, )
+
+                    except:
+                        # print("Unexpected error:", sys.exc_info()[0])
+                        num_errors = num_errors + 1
+                        full_image_name = str(i)
+                        # errors.append(full_image_name + " from "+f)
+                        # pdf.cell(40, 10, "Error adding image " + full_image_name + " from " + f)
+                        # print("Error adding image " + full_image_name + " from " + f)
+                    i = i + 1
         try:
             pdf.close()
-            pdf.output(f + ".pdf", "F")
+            pdf.output("Combo.pdf", "F")
         except:
             print("Error closing/Saving PDF :'(")
 
-if num_errors>0:
-    print("Finished with "+str(num_errors))
-else:
-    print("Finished with no errors :D")
+        if num_errors > 0:
+            print("Finished with " + str(num_errors))
+        else:
+            print("Finished with no errors :D")
+def mainFunc():
+    main(checkBlank.get(), checkCombo.get())
+
+
+
+bf.pack(side=BOTTOM)
+tf.pack()
+checkBox1 = Checkbutton(bf, variable=checkBlank, onvalue=1, offvalue=0, text="First page Blank?").pack()
+checkBox2 = Checkbutton(bf, variable=checkCombo, onvalue=1, offvalue=0, text="Make into one pdf?").pack()
+
+button1 = Button(tf, text="Make PDF", width=45, command=mainFunc)
+button1.pack()
+
+root.mainloop()
+
+print("All done")
